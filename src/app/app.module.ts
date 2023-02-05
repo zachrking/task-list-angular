0efd3f24e809e1b2620868868a1348d3df12a264
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-02-05 02:40:35 -0300

    Generate a new add-form component
    The form's component with the data that will be sent to the backend; button is being used here with a custom name and style class.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { AddFormComponent } from './components/add-form/add-form.component';
 import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
+    AddFormComponent,
     ButtonComponent
   ],
   imports: [
