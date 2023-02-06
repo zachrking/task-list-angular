2c56c1343aac06f64e523a3d40e205dbda41b421
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-02-06 02:53:18 -0300

    Implement a method that post new tasks on the API

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -8,8 +8,17 @@ import { Task } from '../model/task.model';
 })
 export class TaskService {
   private apiUrl: string = "http://localhost:5000/tasks";
+  private httOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
 
   constructor(private http: HttpClient) { }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, this.httOptions);
+  }
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
